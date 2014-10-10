89b85b981798c317c2edba918b31defdffe82082
Kevin Rattai <krattai@gmail.com>
2014-10-10 00:49:26 -0500

    Add c prototype for ctrlwtch.sh replacement

---
+++ core conversion/ctrlwtch.c
@@ -0,0 +1,17 @@
+/* ctrlwtch.c: manages ctrl folder content
+
+Copyright (C) 2014 Uvea I. S., Kevin Rattai
+Date created:	October 10, 2014
+
+*/
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+	printf("\n\nThis applications manages the ctrl folder\n");
+	system("ctrlwtch.sh");
+}
