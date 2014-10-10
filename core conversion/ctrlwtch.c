64ea47d41c39d6115ec378fbcb1e58874c0252f7
Kevin Rattai <krattai@gmail.com>
2014-10-10 09:43:21 -0500

    Added variable assignment protos.

--- core conversion/ctrlwtch.c
+++ core conversion/ctrlwtch.c
@@ -12,6 +12,15 @@ Date created:	October 10, 2014
 
 void main(void)
 {
+    char AEBL_TEST="/home/pi/.aebltest",
+         AEBL_SYS="/home/pi/.aeblsys",
+         TEMP_DIR="/home/pi/tmp",
+         T_STO="/run/shm",
+         T_SCR="/run/shm/scripts",
+         LOCAL_SYS="${T_STO}/.local", /* will be used in conj with path */
+         NETWORK_SYS="${T_STO}/.network", /* will be used in conj with path */
+         OFFLINE_SYS="${T_STO}/.offline"; /* will be used in conj with path */
+
 	printf("\n\nThis applications manages the ctrl folder\n");
 	system("ctrlwtch.sh");
 }
