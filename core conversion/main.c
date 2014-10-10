3b0339740e0bef8ba3c45547da87bb0c809f02f8
Kevin Rattai <krattai@gmail.com>
2014-10-10 18:48:19 -0500

    Moved startup.sh into startup() funtion.

--- core conversion/main.c
+++ core conversion/main.c
@@ -11,6 +11,105 @@ Date created:	October 10, 2014
 #include <stdio.h>
 #include <stdlib.h>
 
+void startup(void)
+{
+
+    /* What follows is the original source from the startup.sh app */
+    /*   Should be part of a main.c function startup()             */
+
+    cd $HOME
+
+    cp -p /home/pi/.scripts/* /run/shm/scripts
+
+    if [ ! -f "${T_STO}/.optimized" ]; then
+    #     sudo service dbus stop
+        sudo mount -o remount,size=128M /dev/shm
+        echo -n performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        sudo service triggerhappy stop
+        sudo killall console-kit-daemon
+        sudo killall polkitd
+        killall gvfsd
+        killall dbus-daemon
+        killall dbus-launch
+        touch $T_STO/.optimized
+
+    fi
+
+    if [ ! -f "${T_STO}/.mkplayrun" ] && [ ! -f "${IHDN_DET}" ]; then
+        $T_SCR/./mkplay.sh &
+    fi
+
+    if [ ! -f "${FIRST_RUN_DONE}" ]; then
+        chmod 777 $HOME/pl
+        chmod 777 $HOME/ctrl
+        chmod 777 $HOME/mp4
+        chmod 777 $HOME/mp3
+
+        rm .id
+
+        # archive files - eventually, this should be a standalone script
+        # assumption made that on first run, all files good
+        mkdir $HOME/.backup
+        cp $HOME/version $HOME/.backup
+        mkdir $HOME/.backup/scripts
+        cp $HOME/scripts/*.sh $HOME/.backup/scripts
+    #     cp $HOME/.scripts/*.sh $HOME/.backup/scripts
+        mkdir $HOME/.backup/bin
+        cp $HOME/bin/* $HOME/.backup/bin
+        mkdir $HOME/.backup/pl
+        cp $HOME/pl/* $HOME/.backup/pl
+        mkdir $HOME/.backup/ctrl
+        cp $HOME/ctrl/* $HOME/.backup/ctrl
+
+        touch $HOME/.firstrundone
+    fi
+
+    if [ -f "${T_STO}/.omx_playing" ]; then
+        rm $T_STO/.omx_playing
+    fi
+
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~" >> log.txt
+    echo $(date +"%T") >> log.txt
+    echo "Booted up." >> log.txt
+
+    # Discover network availability if not previously tested
+    if [ ! -f "${LOCAL_SYS}" ] && [ ! -f "${NETWORK_SYS}" ] && [ ! -f "${OFFLINE_SYS}" ]; then
+
+        $T_SCR/./inetup.sh
+
+    fi
+
+    if [ ! -f "${OFFLINE_SYS}" ] && [ ! -f "${IHDN_DET}" ]; then
+        $T_SCR/./mkuniq.sh &
+
+        ID_FILE="${HOME}/ctrl/ip.txt"
+        IPw0=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+        IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+
+        echo ${IPw0} > ${ID_FILE}
+        echo ${IPe0} >> ${ID_FILE}
+
+        ID_FILE="${HOME}/ctrl/ip.txt"
+        IPw0=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+        IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+
+        echo ${IPw0} > ${ID_FILE}
+        echo ${IPe0} >> ${ID_FILE}
+
+    fi
+
+    if [ ! "$(pgrep ctrlwtch.sh)" ]; then
+        $T_SCR/./ctrlwtch.sh &
+    fi
+
+
+    # clear all network check files
+    rm index*
+
+    $T_SCR/./run.sh &
+
+}
+
 void main(void)
 {
     char AEBL_TEST="/home/pi/.aebltest",
@@ -26,160 +125,66 @@ void main(void)
 	system("startup.sh");
 	system("run.sh");
 
-/* What follows is the original source from the startup.sh app */
-/*   Should be part of a main.c function startup()             */
+    /* What follows is the original source from the run.sh app     */
+    /*   Should be part of a main.c function run() or main()       */
 
-cd $HOME
+    NOTHING_NEW="${T_STO}/.nonew"
+    NEW_PL="${T_STO}/.newpl"
 
-cp -p /home/pi/.scripts/* /run/shm/scripts
-
-if [ ! -f "${T_STO}/.optimized" ]; then
-#     sudo service dbus stop
-    sudo mount -o remount,size=128M /dev/shm
-    echo -n performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-    sudo service triggerhappy stop
-    sudo killall console-kit-daemon
-    sudo killall polkitd
-    killall gvfsd
-    killall dbus-daemon
-    killall dbus-launch
-    touch $T_STO/.optimized
-
-fi
-
-if [ ! -f "${T_STO}/.mkplayrun" ] && [ ! -f "${IHDN_DET}" ]; then
-    $T_SCR/./mkplay.sh &
-fi
-
-if [ ! -f "${FIRST_RUN_DONE}" ]; then
-    chmod 777 $HOME/pl
-    chmod 777 $HOME/ctrl
-    chmod 777 $HOME/mp4
-    chmod 777 $HOME/mp3
-
-    rm .id
-
-    # archive files - eventually, this should be a standalone script
-    # assumption made that on first run, all files good
-    mkdir $HOME/.backup
-    cp $HOME/version $HOME/.backup
-    mkdir $HOME/.backup/scripts
-    cp $HOME/scripts/*.sh $HOME/.backup/scripts
-#     cp $HOME/.scripts/*.sh $HOME/.backup/scripts
-    mkdir $HOME/.backup/bin
-    cp $HOME/bin/* $HOME/.backup/bin
-    mkdir $HOME/.backup/pl
-    cp $HOME/pl/* $HOME/.backup/pl
-    mkdir $HOME/.backup/ctrl
-    cp $HOME/ctrl/* $HOME/.backup/ctrl
-
-    touch $HOME/.firstrundone
-fi
-
-if [ -f "${T_STO}/.omx_playing" ]; then
-    rm $T_STO/.omx_playing
-fi
-
-echo "~~~~~~~~~~~~~~~~~~~~~~~~" >> log.txt
-echo $(date +"%T") >> log.txt
-echo "Booted up." >> log.txt
-
-# Discover network availability if not previously tested
-if [ ! -f "${LOCAL_SYS}" ] && [ ! -f "${NETWORK_SYS}" ] && [ ! -f "${OFFLINE_SYS}" ]; then
-
-    $T_SCR/./inetup.sh
-
-fi
-
-if [ ! -f "${OFFLINE_SYS}" ] && [ ! -f "${IHDN_DET}" ]; then
-    $T_SCR/./mkuniq.sh &
-
-    ID_FILE="${HOME}/ctrl/ip.txt"
     IPw0=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+    MACw0=$(ip link show wlan0 | awk '/ether/ {print $2}')
     IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+    MACe0=$(ip link show eth0 | awk '/ether/ {print $2}')
 
-    echo ${IPw0} > ${ID_FILE}
-    echo ${IPe0} >> ${ID_FILE}
-
-    ID_FILE="${HOME}/ctrl/ip.txt"
-    IPw0=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
-    IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
-
-    echo ${IPw0} > ${ID_FILE}
-    echo ${IPe0} >> ${ID_FILE}
-
-fi
-
-if [ ! "$(pgrep ctrlwtch.sh)" ]; then
-    $T_SCR/./ctrlwtch.sh &
-fi
-
-
-# clear all network check files
-rm index*
-
-$T_SCR/./run.sh &
-
-/* What follows is the original source from the run.sh app     */
-/*   Should be part of a main.c function run() or main()       */
-
-NOTHING_NEW="${T_STO}/.nonew"
-NEW_PL="${T_STO}/.newpl"
-
-IPw0=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
-MACw0=$(ip link show wlan0 | awk '/ether/ {print $2}')
-IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
-MACe0=$(ip link show eth0 | awk '/ether/ {print $2}')
-
-CRONCOMMFILE="${T_STO}/.tempcron"
+    CRONCOMMFILE="${T_STO}/.tempcron"
  
-# set to home directory
+    # set to home directory
  
-cd $HOME
+    cd $HOME
 
-touch $T_STO/.sysrunning
+    touch $T_STO/.sysrunning
 
-while [ -f "${T_STO}/.sysrunning" ]; do
+    while [ -f "${T_STO}/.sysrunning" ]; do
 
-    # log .id
+        # log .id
 
-#    cat .id >> log.txt
+    #    cat .id >> log.txt
 
-    if [ -f "${AEBL_TEST}" ] || [ -f "${AEBL_SYS}" ]; then
-        $T_SCR/./aebl_play.sh
+        if [ -f "${AEBL_TEST}" ] || [ -f "${AEBL_SYS}" ]; then
+            $T_SCR/./aebl_play.sh
 
-        if [ "${MACe0}" == 'b8:27:eb:37:07:5a' ] && [ -f "${AEBL_SYS}" ]; then
-#            echo "MAC is ending :5a so touching .aeblsys_test." >> log.txt
-#            echo $(date +"%T") >> log.txt
-            touch .aeblsys_test
-            touch .aebltest
-            rm ${AEBL_SYS}
+            if [ "${MACe0}" == 'b8:27:eb:37:07:5a' ] && [ -f "${AEBL_SYS}" ]; then
+    #            echo "MAC is ending :5a so touching .aeblsys_test." >> log.txt
+    #            echo $(date +"%T") >> log.txt
+                touch .aeblsys_test
+                touch .aebltest
+                rm ${AEBL_SYS}
+            fi
         fi
-    fi
 
-    if [ -f "${IHDN_TEST}" ] || [ -f "${IHDN_SYS}" ]; then
-        if [ ! -f "${IHDN_DET}" ]; then
-            $T_SCR/./ihdn_play.sh
+        if [ -f "${IHDN_TEST}" ] || [ -f "${IHDN_SYS}" ]; then
+            if [ ! -f "${IHDN_DET}" ]; then
+                $T_SCR/./ihdn_play.sh
+            fi
+            if [ ! -f "${T_STO}/.syschecks" ] && [ ! "$(pgrep ihdn_tests.sh)" ]; then
+                $T_SCR/./ihdn_tests.sh &
+            fi
         fi
-        if [ ! -f "${T_STO}/.syschecks" ] && [ ! "$(pgrep ihdn_tests.sh)" ]; then
-            $T_SCR/./ihdn_tests.sh &
+
+        # Check nothing new
+        # This section to watch and react to requested or auto change in system
+
+
+        if [ -f "${AEBL_TEST}" ] || [ -f "${AEBL_SYS}" ] && [ ! -f "${NOTHING_NEW}" ]; then
+    #        echo "Setting system to not check updates with .nonew" >> log.txt
+    #        echo $(date +"%T") >> log.txt
+            touch $T_STO/.nonew
         fi
-    fi
 
-    # Check nothing new
-    # This section to watch and react to requested or auto change in system
+    done
 
+    # if .sysrunning token cleared, loop back to startup.sh
 
-    if [ -f "${AEBL_TEST}" ] || [ -f "${AEBL_SYS}" ] && [ ! -f "${NOTHING_NEW}" ]; then
-#        echo "Setting system to not check updates with .nonew" >> log.txt
-#        echo $(date +"%T") >> log.txt
-        touch $T_STO/.nonew
-    fi
-
-done
-
-# if .sysrunning token cleared, loop back to startup.sh
-
-$T_SCR/./startup.sh &
+    $T_SCR/./startup.sh &
 
 }
