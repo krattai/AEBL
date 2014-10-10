2e635da6da22fda4d54e858d54bfba1fd0b3f949
Kevin Rattai <krattai@gmail.com>
2014-10-10 10:26:27 -0500

    Added startup.sh as proto to be used as startup() function.

--- core conversion/main.c
+++ core conversion/main.c
@@ -26,4 +26,98 @@ void main(void)
 	system("startup.sh");
 	system("run.sh");
 
+/* What follows is the original source from the startup.sh app */
+/*   Should be part of a main.c function startup()             */
+
+cd $HOME
+
+cp -p /home/pi/.scripts/* /run/shm/scripts
+
+if [ ! -f "${T_STO}/.optimized" ]; then
+#     sudo service dbus stop
+    sudo mount -o remount,size=128M /dev/shm
+    echo -n performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    sudo service triggerhappy stop
+    sudo killall console-kit-daemon
+    sudo killall polkitd
+    killall gvfsd
+    killall dbus-daemon
+    killall dbus-launch
+    touch $T_STO/.optimized
+
+fi
+
+if [ ! -f "${T_STO}/.mkplayrun" ] && [ ! -f "${IHDN_DET}" ]; then
+    $T_SCR/./mkplay.sh &
+fi
+
+if [ ! -f "${FIRST_RUN_DONE}" ]; then
+    chmod 777 $HOME/pl
+    chmod 777 $HOME/ctrl
+    chmod 777 $HOME/mp4
+    chmod 777 $HOME/mp3
+
+    rm .id
+
+    # archive files - eventually, this should be a standalone script
+    # assumption made that on first run, all files good
+    mkdir $HOME/.backup
+    cp $HOME/version $HOME/.backup
+    mkdir $HOME/.backup/scripts
+    cp $HOME/scripts/*.sh $HOME/.backup/scripts
+#     cp $HOME/.scripts/*.sh $HOME/.backup/scripts
+    mkdir $HOME/.backup/bin
+    cp $HOME/bin/* $HOME/.backup/bin
+    mkdir $HOME/.backup/pl
+    cp $HOME/pl/* $HOME/.backup/pl
+    mkdir $HOME/.backup/ctrl
+    cp $HOME/ctrl/* $HOME/.backup/ctrl
+
+    touch $HOME/.firstrundone
+fi
+
+if [ -f "${T_STO}/.omx_playing" ]; then
+    rm $T_STO/.omx_playing
+fi
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~" >> log.txt
+echo $(date +"%T") >> log.txt
+echo "Booted up." >> log.txt
+
+# Discover network availability if not previously tested
+if [ ! -f "${LOCAL_SYS}" ] && [ ! -f "${NETWORK_SYS}" ] && [ ! -f "${OFFLINE_SYS}" ]; then
+
+    $T_SCR/./inetup.sh
+
+fi
+
+if [ ! -f "${OFFLINE_SYS}" ] && [ ! -f "${IHDN_DET}" ]; then
+    $T_SCR/./mkuniq.sh &
+
+    ID_FILE="${HOME}/ctrl/ip.txt"
+    IPw0=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+    IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+
+    echo ${IPw0} > ${ID_FILE}
+    echo ${IPe0} >> ${ID_FILE}
+
+    ID_FILE="${HOME}/ctrl/ip.txt"
+    IPw0=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+    IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+
+    echo ${IPw0} > ${ID_FILE}
+    echo ${IPe0} >> ${ID_FILE}
+
+fi
+
+if [ ! "$(pgrep ctrlwtch.sh)" ]; then
+    $T_SCR/./ctrlwtch.sh &
+fi
+
+
+# clear all network check files
+rm index*
+
+$T_SCR/./run.sh &
+
 }
