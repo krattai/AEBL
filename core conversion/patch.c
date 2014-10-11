8cd06a4ca694bf833c139ec6ec0ee602de200abe
Kevin Rattai <krattai@gmail.com>
2014-10-10 19:38:36 -0500

    Added dopatch.sh to patch.c as proto and probable dopatch() function

--- core conversion/patch.c
+++ core conversion/patch.c
@@ -145,4 +145,79 @@ void startup(void)
         echo "system offline, cannot patch"
     fi
 
+    /* What follows is the original source from the dopatch.sh app */
+
+    LOCAL_SYS="/home/pi/.local"
+    NETWORK_SYS="/home/pi/.network"
+    OFFLINE_SYS="/home/pi/.offline"
+    AEBL_SYS="/home/pi/.aeblsys"
+
+    TEMP_DIR="/home/pi/tempdir"
+    MP3_DIR="/home/pi/mp3"
+    MP4_DIR="/home/pi/mp4"
+    PL_DIR="/home/pi/pl"
+    CTRL_DIR="/home/pi/ctrl"
+    BIN_DIR="/home/pi/bin"
+
+    USER=`whoami`
+    CRONLOC=/var/spool/cron/crontabs
+    # CRONCOMMFILE=/tmp/cron_comm_file.sh
+    CRONCOMMFILE="${HOME}/testcron.sh"
+    CRONGREP=$(crontab -l | cat )
+
+    IPe0=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+    MACe0=$(ip link show eth0 | awk '/ether/ {print $2}')
+
+    cd $HOME
+
+    # Discover network availability
+    #
+
+    ping -c 1 8.8.8.8
+
+    if [ $? -eq 0 ]; then
+        touch .network
+        echo "Internet available."
+    else
+        rm .network
+    fi
+
+    ping -c 1 192.168.200.6
+
+    if [[ $? -eq 0 ]]; then
+        touch .local
+        echo "Local network available."
+    else
+        rm .local
+    fi
+
+    if [ ! -f "${LOCAL_SYS}" ] && [ ! -f "${NETWORK_SYS}" ]; then
+        touch .offline
+        echo "No network available."
+    else
+        rm .offline
+    fi
+
+    touch ${AEBL_SYS}
+
+    export PATH=$PATH:${BIN_DIR}:$HOME/scripts
+
+    mv patch.sh $HOME/scripts
+    chmod 777 $HOME/scripts/patch.sh
+    cp $HOME/scripts/patch.sh /run/shm/scripts
+
+    mv upgrade.sh $HOME/scripts
+    chmod 777 $HOME/scripts/upgrade.sh
+    cp $HOME/scripts/upgrade.sh /run/shm/scripts
+
+    chmod 777 cronadd.sh
+    ./cronadd.sh
+
+    GRAB_FILE="pv"
+    pv=$(cat "${GRAB_FILE}" | head -n1)
+
+    if [ ! -f "${OFFLINE_SYS}" ]; then
+        $HOME/tmpdir_maintenance/mod_Twitter/./tcli.sh -c statuses_update -s "automagic @kratt, ${MACe0} patched to ${pv}." &
+    fi
+
 }
