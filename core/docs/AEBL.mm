dc3410a78fc4639753c5891146e48377b70c2063
Kevin Rattai <krattai@gmail.com>
2015-01-02 13:47:53 -0600

    Added twython node to twitterbot branch

--- core/docs/AEBL.mm
+++ core/docs/AEBL.mm
@@ -109,6 +109,7 @@
 <node TEXT="bash ready" ID="ID_589979534" CREATED="1420227641286" MODIFIED="1420227653411"/>
 <node TEXT="tcli.zip contains files" ID="ID_1282589937" CREATED="1420227694641" MODIFIED="1420227705798"/>
 </node>
+<node TEXT="twython" ID="ID_1318675621" CREATED="1420228047593" MODIFIED="1420228064620" LINK="https://github.com/ryanmcgrath/twython"/>
 </node>
 </node>
 <node TEXT="Prototype Showcasing" ID="Freemind_Link_196559201" CREATED="1269462299880" MODIFIED="1269462316023">
