e5c27569ead4a5038420be835e8534a529376eef
Kevin Rattai <krattai@gmail.com>
2015-01-03 00:46:30 -0600

    Fixed AEBL_VM logic problems

--- installer/aeblv0091/casys0091.sh
+++ installer/aeblv0091/casys0091.sh
@@ -181,11 +181,11 @@ if [ ! -f "${OFFLINE_SYS}" ]; then
 
     cp -p ${SCRPT_DIR}/* /run/shm/scripts
 
-if [ -f "$HOME/aeblvm" ]; then
-    $HOME/tmpdir_maintenance/mod_Twitter/./tcli.sh -c statuses_update -s "automagic @kratt, #AEBL_VM ${MACe0} registered." &
-else
-    $HOME/tmpdir_maintenance/mod_Twitter/./tcli.sh -c statuses_update -s "automagic @kratt, #AEBLpi ${MACe0} registered." &
-fi
+    if [ -f "${AEBL_VM}" ]; then
+        $HOME/tmpdir_maintenance/mod_Twitter/./tcli.sh -c statuses_update -s "automagic @kratt, #AEBL_VM ${MACe0} registered." &
+    else
+        $HOME/tmpdir_maintenance/mod_Twitter/./tcli.sh -c statuses_update -s "automagic @kratt, #AEBLpi ${MACe0} registered." &
+    fi
     # sleep 5 seconds to ensure system ready for reboot
     echo "Processing files.  Please wait."
     sleep 15
@@ -199,7 +199,9 @@ fi
 rm /home/pi/.scripts/asys0091.sh
 
 # aeblvm should be set to .noauto
-touch /home/pi/.noauto
+if [ -f "${AEBL_VM}" ]; then
+    touch /home/pi/.noauto
+fi
 
 sudo reboot
 
