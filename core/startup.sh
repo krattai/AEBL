f8462dec25b8699b0b3537b8133ff9748be51f60
Kevin Rattai <krattai@gmail.com>
2015-01-22 14:32:17 -0600

    Added license link to startup.sh

--- core/startup.sh
+++ core/startup.sh
@@ -2,6 +2,7 @@
 # gets update scripts
 #
 # Copyright (C) 2015 Uvea I. S., Kevin Rattai
+# BSD license https://raw.githubusercontent.com/krattai/AEBL/master/LICENSE
 #
 # This is the first script from clean bootup.  It should immediately
 # put something to screen and audio so that people know it is working,
