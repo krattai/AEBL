98afb3062241262b19531e2715fcc596511d5ed0
Kevin Rattai <krattai@gmail.com>
2015-01-25 11:55:51 -0600

    Created base patch_cgi.sh to force appliance patch process

---
+++ core/base_iface/cgi-bin/patch_cgi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# provides web interface control to shut down computer
+#
+# Copyright (C) 2015 Uvea I. S., Kevin Rattai
+# BSD license https://raw.githubusercontent.com/krattai/AEBL/master/LICENSE
+#
+
+AEBL_TEST="/home/pi/.aebltest"
+AEBL_SYS="/home/pi/.aeblsys"
+TEMP_DIR="/home/pi/tmp"
+IHDN_TEST="/home/pi/.ihdntest"
+IHDN_SYS="/home/pi/.ihdnsys"
+IHDN_DET="/home/pi/.ihdndet"
+
+T_STO="/run/shm"
+T_SCR="/run/shm/scripts"
+
+LOCAL_SYS="${T_STO}/.local"
+NETWORK_SYS="${T_STO}/.network"
+OFFLINE_SYS="${T_STO}/.offline"
+
+
+# it is this simple, but MUST be done as user pi
+sudo -u pi touch /home/pi/ctr/halt
+
+exit
