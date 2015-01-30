7e5afe63537633a7579dc5c4c98aad4441ed644b
Kevin Rattai <krattai@gmail.com>
2015-01-30 00:34:16 -0600

    Fixed apt-get to use -y

--- core/patches/ap009112/dopatch.sh
+++ core/patches/ap009112/dopatch.sh
@@ -66,7 +66,7 @@ fi
 
 # install on AEBL VM appliances and also enable cgi-mod
 if [ -f "${AEBL_VM}" ]; then
-    sudo apt-get install build-essential
+    sudo apt-get -y install build-essential
     sudo a2enmod cgi
     sudo service apache2 restart
 fi
