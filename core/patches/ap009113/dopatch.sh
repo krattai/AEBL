071a5f79eb7d41e44ab925926d5732dd5d4b1552
Kevin Rattai <krattai@gmail.com>
2015-01-30 23:17:52 -0600

    Updated doptach to move and set html, cgi, and general scripts

--- core/patches/ap009113/dopatch.sh
+++ core/patches/ap009113/dopatch.sh
@@ -157,15 +157,22 @@ else
     sudo mv about.html /var/www/html/about.html
 fi
 
-mv startup.sh $HOME/.scripts
-chmod 777 $HOME/.scripts/startup.sh
-cp $HOME/.scripts/startup.sh $HOME/.backup/scripts
-cp $HOME/.scripts/startup.sh /run/shm/scripts
+sudo mv patch_cgi.sh /usr/lib/cgi-bin/
+sudo chmod 777 /usr/lib/cgi-bin/patch_cgi.sh
+sudo mv reboot_cgi.sh /usr/lib/cgi-bin/
+sudo chmod 777 /usr/lib/cgi-bin/reboot_cgi.sh
+sudo mv showcfg_cgi.sh /usr/lib/cgi-bin/
+sudo chmod 777 /usr/lib/cgi-bin/showcfg_cgi.sh
+sudo mv shutdown_cgi.sh /usr/lib/cgi-bin/
+sudo chmod 777 /usr/lib/cgi-bin/shutdown_cgi.sh
+sudo mv sysview_cgi.sh /usr/lib/cgi-bin/
+sudo chmod 777 /usr/lib/cgi-bin/sysview_cgi.sh
 
-mv l-ctrl.sh $HOME/.scripts
-chmod 777 $HOME/.scripts/l-ctrl.sh
-cp $HOME/.scripts/l-ctrl.sh $HOME/.backup/scripts
-cp $HOME/.scripts/l-ctrl.sh /run/shm/scripts
+# Add or update general scripts
+mv ctrlwtch.sh $HOME/.scripts
+chmod 777 $HOME/.scripts/ctrlwtch.sh
+cp $HOME/.scripts/ctrlwtch.sh $HOME/.backup/scripts
+cp $HOME/.scripts/ctrlwtch.sh /run/shm/scripts
 
 mv prs.sh $HOME/.scripts
 chmod 777 $HOME/.scripts/prs.sh
