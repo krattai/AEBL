33f6550b809ab4abcaa7902d7c622240386d03b0
Kevin Rattai <krattai@gmail.com>
2015-10-25 22:47:39 -0500

    Added notes to address purpose and usage of msgrec.sh

--- core/msgrec.sh
+++ core/msgrec.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+# AEBL receive message using MQTT
+#
+# Copyright (C) 2015 Uvea I. S., Kevin Rattai
+#
 
 i="0"
 
+# should modify this loop to simply listen persistently
 while [ $i -lt 9999 ]
 do
 ext_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 # mosquitto_pub -d -t hello/world -m "$(date) : irot LdB, online. IP is $ext_ip" -h "uveais.ca"
-mosquitto_pub -d -t hello/world -m "$(date) : device IP $ext_ip is online." -h "uveais.ca"
+
+mosquitto_pub -d -t hello/world -m "$(date) : AEBL device from $ext_ip ping." -h "uveais.ca"
+
+# the following code is example of MQTT subscribe usage
+# mosquitto_sub -h uveais.ca -t "hello/+" -t "aebl/+" -t "ihdn/+"
+
+# this routine function should listen on topic and then act on it
+
 i=$[$i+1]
 sleep 300
 done
