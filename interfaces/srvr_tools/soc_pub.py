ae97026d0f4c4f2a6cec3f5c96154c6f2e6be4ed
Kevin Rattai <krattai@gmail.com>
2016-02-17 14:54:24 -0600

    Added srvr_tools directory and soc_pub.py social publishing test program

---
+++ interfaces/srvr_tools/soc_pub.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import paho.mqtt.client as mqtt
+
+# Unknown license, code found here:  http://stackoverflow.com/questions/3177545$
+
+# reference to MQTT python found here: http://mosquitto.org/documentation/pytho$
+
+# requires:  sudo pip install paho-mqtt
+# pip requires: sudo apt-get install python-pip
+
+message = 'ON'
+def on_connect(mosq, obj, rc):
+# subscribe([("my/topic", 0), ("another/topic", 2)])
+#    mqttc.subscribe("aebl/hello", 0)
+#    mqttc.subscribe("aebl/alive", 0)
+    mqttc.subscribe("uvea/world", 0)
+    print("rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    global message
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    message = msg.payload
+#    mqttc.publish("uvea/world",msg.payload);
+    if 'ACK' in message:
+        mqttc.publish("uvea/world","NAK");
+#    mqttcb.publish("uvea/world",msg.payload);
+
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mosq, obj, level, string):
+    print(string)
+
+mqttc = mqtt.Client()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+# Connect
+# mqttc.connect("localhost", 1883,60)
+
+mqttc.connect("2001:5c0:1100:dd00:240:63ff:fefd:d3f1", 1883,60)
+# mqttc.connect("2001:5c0:1100:dd00:ba27:ebff:fe2c:41d7", 1883,60)
+
+# mosquitto_sub -h 2001:5c0:1100:dd00:ba27:ebff:fe2c:41d7 -t "hello/+" -t "aebl$
+
+# Continue the network loop
+mqttc.loop_forever()
+
