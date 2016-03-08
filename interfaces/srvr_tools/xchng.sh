82d120226777dc15274b1f49ef3562b80726d47e
Kevin Rattai <krattai@gmail.com>
2016-03-08 01:58:11 -0600

    Updated xchng.sh to get silver price and published message

--- interfaces/srvr_tools/xchng.sh
+++ interfaces/srvr_tools/xchng.sh
@@ -3,9 +3,13 @@
 wget -O btccad.txt "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=BTCCAD=X"
 wget -O usdcad.txt "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=USDCAD=X"
 wget -O gldcad.txt "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=XAUCAD=X"
+wget -O slvcad.txt "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=XAGCAD=X"
 
 btc=$(awk -F "\"*,\"*" '{print $2}' btccad.txt)
 usd=$(awk -F "\"*,\"*" '{print $2}' usdcad.txt)
 gld=$(awk -F "\"*,\"*" '{print $2}' gldcad.txt)
+slv=$(awk -F "\"*,\"*" '{print $2}' slvcad.txt)
 
-mosquitto_pub -d -t aebl/social -m "Today's bitcoin rate CAD\\\$$btc and USD rate CAD\\\$$usd and gold rate CAD\\\$$gld #PSA #am2p" -h "uveais.ca"
+mosquitto_pub -d -t aebl/social -m "Today: bitcoin - CAD\\\$$btc, USD - CAD\\\$$usd, silver - CAD\\\$$slv, gold - CAD\\\$$gld & 1lb flour still a loaf of bread #PSA #am2p" -h "uveais.ca"
+
+exit 0
