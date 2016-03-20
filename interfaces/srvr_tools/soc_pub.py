56c2cf1313ec39a32cc0c1a81e2a3c8d230057d8
Kevin Rattai <krattai@gmail.com>
2016-03-20 18:08:41 -0500

    Removed a comment line that was incorrect

--- interfaces/srvr_tools/soc_pub.py
+++ interfaces/srvr_tools/soc_pub.py
@@ -69,8 +69,6 @@ mqttc.on_subscribe = on_subscribe
 # mqttc.connect("2001:5c0:1100:dd00:ba27:ebff:fe2c:41d7", 1883,60)
 mqttc.connect("uveais.ca", 1883,60)
 
-# mosquitto_sub -h 2001:5c0:1100:dd00:ba27:ebff:fe2c:41d7 -t "hello/+" -t "aebl$
-
 # Continue the network loop
 mqttc.loop_forever()
 
