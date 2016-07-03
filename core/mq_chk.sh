119f593247df560656dac00e6e009928b1041546
Kevin Rattai <krattai@gmail.com>
2016-07-03 13:07:12 -0500

    Added mq_chk.sh for testing message response

---
+++ core/mq_chk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 Uvea I. S., Kevin Rattai
+# checks for control messages and acts accordingly
+#
+# This script borrows from:
+#
+# Element14 - SuddenImpact design challenge
+# http://www.element14.com/community/community/design-challenges/sudden-impact
+#
+# This file is part of submission made for Modular&EasyConfigure project (tomaj$
+#
+# v010 - January 11th 2015.
+#      Initial version. Subscribe to MQTT broker and act upon messages received.
+#
+#
+#
+
+mosquitto_sub -h 2001:5c0:1100:dd00:240:63ff:fefd:d3f1 -t "hello/+" -t "aebl/+" -t "ihdn/+" -t "uvea/+" |
+while IFS= read -r line
+    do
+          if [[ $line = "sixxs alive" ]]; then
+              echo "$(date +"%T") - sixxs ACK"
+              echo " "
+          fi
+done
+
+exit 0
