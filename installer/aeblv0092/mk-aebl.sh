602fe7d50de7ceec72b77986d80c24efa5422a6b
Kevin Rattai <krattai@gmail.com>
2016-07-25 12:39:57 -0500

    Added check to wait for network or 5 minutes before installing or end

--- installer/aeblv0092/mk-aebl.sh
+++ installer/aeblv0092/mk-aebl.sh
@@ -43,14 +43,26 @@ fi
 # Discover network availability
 #
 
-ping -c 1 8.8.8.8
+net_wait=0
 
-if [ $? -eq 0 ]; then
-    touch .network
-    echo "Internet available."
-else
-    rm .network
-fi
+# Repeat for 5 minutes, or 5 cycles, until network available or still no network
+while [ ! -f "${NETWORK_SYS}" ] && [ $net_wait < 10 ]; do
+
+    # is google there?
+    ping -c 1 8.8.8.8
+
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # use this as reference for future feature to grab install file immediately from net
+    if [ $? -eq 0 ]; then
+        touch $NETWORK_SYS
+        echo "Internet available."
+    else
+        rm $NETWORK_SYS
+        net_wait=net_wait+1
+        sleep 30
+    fi
+
+done
 
 if [ ! -f "${LOCAL_SYS}" ] && [ ! -f "${NETWORK_SYS}" ]; then
     touch .offline
