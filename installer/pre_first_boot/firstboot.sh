acf1d067a355de80371c24318a1d131d8f13a006
Kevin Rattai <krattai@gmail.com>
2016-07-25 12:06:27 -0500

    Fixed wrongly named counter

--- installer/pre_first_boot/firstboot.sh
+++ installer/pre_first_boot/firstboot.sh
@@ -64,7 +64,7 @@ while [ ! -f "${NETWORK_SYS}" ] && [ $net_wait < 10 ]; do
         wget -N -nd -w 3 -P scripts --limit-rate=50k https://www.dropbox.com/s/56wti4xtbu4txf4/create-aebl.sh
     else
         rm $NETWORK_SYS
-        net_wait=netwait+1
+        net_wait=net_wait+1
         sleep 30
     fi
 
