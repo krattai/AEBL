d0f728b4f87f5c16f89b1ee9740b7ac1d27deb1a
Kevin Rattai <krattai@gmail.com>
2016-08-23 23:12:26 -0500

    Updated for possible cdn processing

--- broker/cdn.sh
+++ broker/cdn.sh
@@ -5,7 +5,7 @@
 #
 # Useage:
 # There is no useage, this is a standalone script
-
+#
 # For certain functions, use case:
 #
 #
@@ -36,16 +36,37 @@ i="0"
 
 while [ $i -lt 1 ]
 do
-hostn=$(cat /etc/hostname)
-ext_ip4=$(dig +short myip.opendns.com @resolver1.opendns.com)
-ext_ip6=$(curl icanhazip.com)
+    mosquitto_sub -h uveais.ca -t -t "aebl/#" -t "uvea/#" |
+    while IFS= read -r line
+        do
+#           if [[ $line = "sixxs alive" ]]; then
+#               echo "$(date +"%T") - sixxs ACK"
+#               echo " "
+#           fi
+#           if [[ $line == *"ihdnsrvr IPv6"* ]]; then
+#               echo "$(date +"%T") - ihdnsrvr ACK"
+#               echo "$line"
+#               echo " "
+#           fi
+#           if [[ $line == *"played"* ]]; then
+#               echo "$(date +"%T") - play log"
+#               echo "$line"
+#               echo " "
+#           fi
+#
+        done
+
+# hostn=$(cat /etc/hostname)
+# ext_ip4=$(dig +short myip.opendns.com @resolver1.opendns.com)
+# ext_ip6=$(curl icanhazip.com)
 # mosquitto_pub -d -t hello/world -m "$(date) : irot LdB, online. IP is $ext_ip" -h "uveais.ca"
 # mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn server IP $ext_ip is online." -h "2604:8800:100:19a::2"
 # mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn IPv4 $ext_ip4 is online." -h "2001:5c0:1100:dd00:240:63ff:fefd:d3f1"
 # mosquitto_pub -d -t aebl/alive -m "$(date) : $hostn IPv6 $ext_ip6 is online." -h "2001:5c0:1100:dd00:240:63ff:fefd:d3f1"
-mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn IPv4 $ext_ip4 is online." -h "ihdn.ca"
-mosquitto_pub -d -t aebl/alive -m "$(date) : $hostn IPv6 $ext_ip6 is online." -h "uveais.ca"
+# mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn IPv4 $ext_ip4 is online." -h "ihdn.ca"
+# mosquitto_pub -d -t aebl/alive -m "$(date) : $hostn IPv6 $ext_ip6 is online." -h "uveais.ca"
 # i=$[$i+1]
-sleep 300
+# sleep 300
 done
 
+exit 0
