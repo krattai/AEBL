af035c467feb43ce933d200a5024f0db5a435cfd
Kevin Rattai <krattai@gmail.com>
2016-09-14 10:13:19 -0500

    Added remarks for copyright and function

--- core/alpha/pip.sh
+++ core/alpha/pip.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+# This script should auto pub content from flat file
+# Copyright (C) 2016 Uvea I. S., Kevin Rattai
+#
+# Plays pip (Picture In Picture)
+#
+# Audio should be off for these inserts.
+#
+
 mkfifo fifo
 chmod 777 fifo
 omxplayer --layer 2 --win "475 300 650 400" "mp4/Revolution OS.mp4" <fifo&
