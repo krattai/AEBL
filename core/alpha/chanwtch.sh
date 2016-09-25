23a0a94f3d115e0fb3d5036deb51760e56e07459
Kevin Rattai <krattai@gmail.com>
2016-09-25 12:16:15 -0500

    Updated script with examples

--- core/alpha/chanwtch.sh
+++ core/alpha/chanwtch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
 # Need to specific /bin/bash because /bin/sh (=dash) doesn't like redirect
+
+# Copyright (C) 2015 - 2016 Uvea I. S., Kevin Rattai
+
 #
 # Element14 - SuddenImpact design challenge
 # http://www.element14.com/community/community/design-challenges/sudden-impact
@@ -16,6 +19,20 @@
 mosquitto_sub -h 2001:5c0:1100:dd00:240:63ff:fefd:d3f1 -t "hello/+" -t "aebl/+" -t "ihdn/+" -t "uvea/+" |
 while IFS= read -r line
     do
+#           if [[ $line = "sixxs alive" ]]; then
+#               echo "$(date +"%T") - sixxs ACK"
+#               echo " "
+#           fi
+#           if [[ $line == *"ihdnsrvr IPv6"* ]]; then
+#               echo "$(date +"%T") - ihdnsrvr ACK"
+#               echo "$line"
+#               echo " "
+#           fi
+#
+
+            # Append file to playlist
+#             echo "$line #am2p" >> "${CONTENT}"
+
           if [[ $line = "sixxs alive" ]]; then
               echo "$(date +"%T") - sixxs ACK"
               echo " "
