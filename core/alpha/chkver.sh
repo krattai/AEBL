e290a24dd8d1f022e9b6238a2f0da46ac5ba8429
Kevin Rattai <krattai@gmail.com>
2016-10-02 14:25:18 -0500

    Added reference URL and more checks for Pi models

--- core/alpha/chkver.sh
+++ core/alpha/chkver.sh
@@ -4,19 +4,43 @@
 #
 # Useage:
 # There is no useage, this is a standalone script
+#
+# Models as indicated here:
+# http://www.raspberrypi-spy.co.uk/2012/09/checking-your-raspberry-pi-board-version/
 
 cat /proc/cpuinfo | grep a01041
 
 if [ $? -eq 0 ]; then
     touch .p2
-    echo "Sony Pi 2."
+    echo "Sony Pi 2 B."
 fi
 
 cat /proc/cpuinfo | grep a21041
 
 if [ $? -eq 0 ]; then
     touch .p2
-    echo "Embest Pi 2."
+    echo "Embest Pi 2 B."
+fi
+
+cat /proc/cpuinfo | grep 900092
+
+if [ $? -eq 0 ]; then
+    touch .p2
+    echo "Pi Zero."
+fi
+
+cat /proc/cpuinfo | grep a02082
+
+if [ $? -eq 0 ]; then
+    touch .p2
+    echo "Sony Pi 3 B."
+fi
+
+cat /proc/cpuinfo | grep a22082
+
+if [ $? -eq 0 ]; then
+    touch .p2
+    echo "Embest Pi 3 B."
 fi
 
 exit
