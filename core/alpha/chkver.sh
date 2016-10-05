b4f6e4db724cf707af1ced77eb1626640b2f7253
Kevin Rattai <krattai@gmail.com>
2016-10-04 23:27:45 -0500

    Added var to collect ver and constructed p2, p3, and known pi model b checks

--- core/alpha/chkver.sh
+++ core/alpha/chkver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# This script checks version for Pi 2
+# This script checks version for Pi version
 # Copyright (C) 2016 Uvea I. S., Kevin Rattai
 #
 # Useage:
@@ -14,41 +14,79 @@
 # Command to pull rev info, per elinux:
 # + cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'
 
+# Try this:
+# cat /proc/cpuinfo | grep a01041
+# 
+# if [ $? -eq 0 ]; then
+#     touch .p2
+#     echo "Sony Pi 2 B."
+# fi
 
-cat /proc/cpuinfo | grep a01041
+# cat /proc/cpuinfo | grep a21041
+# 
+# if [ $? -eq 0 ]; then
+#     touch .p2
+#     echo "Embest Pi 2 B."
+# fi
 
-if [ $? -eq 0 ]; then
+# cat /proc/cpuinfo | grep 900092
+
+# if [ $? -eq 0 ]; then
+#     touch .p2
+#     echo "Pi Zero."
+# fi
+
+# cat /proc/cpuinfo | grep a02082
+
+# if [ $? -eq 0 ]; then
+#     touch .p2
+#     echo "Sony Pi 3 B."
+# fi
+
+# cat /proc/cpuinfo | grep a22082
+
+# if [ $? -eq 0 ]; then
+#     touch .p2
+#     echo "Embest Pi 3 B."
+# fi
+
+ver=$(cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//')
+
+if [[ $ver = "a01040" ]]; then
+    touch .p2
+    echo "Unknown Pi 2 B."
+fi
+
+if [[ $ver = "a01041" ]]; then
     touch .p2
     echo "Sony Pi 2 B."
 fi
 
-cat /proc/cpuinfo | grep a21041
-
-if [ $? -eq 0 ]; then
+if [[ $ver = "a21041" ]]; then
     touch .p2
     echo "Embest Pi 2 B."
 fi
 
-cat /proc/cpuinfo | grep 900092
-
-if [ $? -eq 0 ]; then
+if [[ $ver = "a22042" ]]; then
     touch .p2
-    echo "Pi Zero."
+    echo "Embest Pi 2 B with BCM2837."
 fi
 
-cat /proc/cpuinfo | grep a02082
-
-if [ $? -eq 0 ]; then
-    touch .p2
+if [[ $ver = "a02082" ]]; then
+    touch .p3
     echo "Sony Pi 3 B."
 fi
 
-cat /proc/cpuinfo | grep a22082
-
-if [ $? -eq 0 ]; then
-    touch .p2
+if [[ $ver = "a22082" ]]; then
+    touch .p3
     echo "Embest Pi 3 B."
 fi
+
+if [[ $ver = "000e" ]]; then
+    touch .p1
+    echo "Sony Pi 1 B Q4 2012."
+fi
+
 
 exit
 
