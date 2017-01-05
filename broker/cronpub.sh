874a615602446789bd8e16e14de165f21d0a1bc2
Kevin Rattai <krattai@gmail.com>
2017-01-05 17:09:17 -0600

    Updated cronpub to tail and cat rotating content

--- broker/cronpub.sh
+++ broker/cronpub.sh
@@ -2,8 +2,10 @@
 # This script should auto pub content from flat file
 # Copyright (C) 2017 Uvea I. S., Kevin Rattai
 #
-# Runs as cron job, should post one item per day
-
+# Runs as cron job, will post one item per day at this time, with more possible in future
+#
+# cron as:
+# 0     14     *     *     * /home/kevin/scripts/cronpub.sh
 #
 # Use AEBL playlist.sh and process_playlist.sh for example code
 #  to process files
@@ -14,9 +16,6 @@ CONT_DIR="/home/kevin/content"
 # PLAYLIST="${T_STO}/.playlist"
 CONTENT="${CONT_DIR}/.soc_content"
 
-# PLAYLIST="${T_STO}/.playlist"
-PLAYED="${CONT_DIR}/.soc_played"
-
 # Get the top of the playlist
 pub=$(cat "${CONTENT}" | head -n1)
 
@@ -26,7 +25,7 @@ mv "${CONTENT}.new" "${CONTENT}"
 
 mosquitto_pub -d -t aebl/social -m "$pub" -h "ihdn.ca"
 
-# Append pub to old content
-echo "$pub" >> "${PLAYED}"
+# Append pub to end of content
+echo "$pub" >> "${CONTENT}"
 
 exit 0
