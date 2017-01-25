6334e0c16ed523df11bd08d933280409b532ff79
Kevin Rattai <krattai@gmail.com>
2017-01-25 16:05:52 -0600

    Added nextcloud for reference and possible owncloud replacement

---
+++ blades/nextcloud_server/apps/files_versions/l10n/el.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "files_versions",
+    {
+    "Could not revert: %s" : "Αδυναμία επαναφοράς: %s",
+    "Versions" : "Εκδόσεις",
+    "Failed to revert {file} to revision {timestamp}." : "Αποτυχία επαναφοράς του {file} στην αναθεώρηση {timestamp}.",
+    "Restore" : "Επαναφορά",
+    "More versions..." : "Περισσότερες εκδόσεις...",
+    "No other versions available" : "Δεν υπάρχουν άλλες εκδόσεις διαθέσιμες"
+},
+"nplurals=2; plural=(n != 1);");
