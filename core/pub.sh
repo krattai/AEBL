90be4b2161ac541a0f3b256a136a830c43a742ab
Kevin Rattai <krattai@gmail.com>
2017-01-31 15:34:40 -0600

    Changed to pub on host specific channel

--- core/pub.sh
+++ core/pub.sh
@@ -12,8 +12,8 @@ ext_ip6=$(curl icanhazip.com)
 # mosquitto_pub -d -t ihdn/alive -m "$(date) : rotator SIXXS device IP $ext_ip4 is online." -h "2604:8800:100:19a::2"
 # mosquitto_pub -d -t ihdn/alive -m "$(date) : rotator SIXXS device IP $ext_ip6 is online." -h "2001:5c0:1100:dd00:240:63ff:fefd:d3f1"
 
-mosquitto_pub -d -t ihdn/sixxs -m "$(date) : sixxs $hostn IPv4 $ext_ip4 is online." -h "ihdn.ca"
-mosquitto_pub -d -t ihdn/sixxs -m "$(date) : sixxs $hostn IPv6 $ext_ip6 is online." -h "ihdn.ca"
+mosquitto_pub -d -t ihdn/$hostn -m "$(date) : sixxs $hostn IPv4 $ext_ip4 is online." -h "ihdn.ca"
+mosquitto_pub -d -t ihdn/$hostn -m "$(date) : sixxs $hostn IPv6 $ext_ip6 is online." -h "ihdn.ca"
 
 IPt0=$(ip addr show tun0 | awk '/inet / {print $2}' | cut -d/ -f 1)
 mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn tun0 $IPt0 is online." -h "ihdn.ca"
