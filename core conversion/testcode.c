6e3f4d5450c7428eb42f8128c3ddd9e5965a0767
Kevin Rattai <krattai@gmail.com>
2017-04-03 23:58:59 -0500

    Added var and const def syntax for testing

--- core conversion/testcode.c
+++ core conversion/testcode.c
@@ -12,9 +12,30 @@ Date created: April 3, 2017
 #include <sys/types.h>
 #include <unistd.h>
 
-
 int main ()
 {
+/* var and const defs */
+
+    const char AEBL_TEST[] = "/home/pi/.aebltest",
+               AEBL_SYS[] = "/home/pi/.aeblsys",
+               TEMP_DIR[] = "/home/pi/tmp",
+               T_STO[] = "/run/shm",
+               T_SCR[] = "/run/shm/scripts",
+               LOCAL_SYS[] = "${T_STO}/.local", /* will be used in conj with path */
+               NETWORK_SYS[] = "${T_STO}/.network", /* will be used in conj with path */
+               OFFLINE_SYS[] = "${T_STO}/.offline"; /* will be used in conj with path */
+
+
+    const char NOTHING_NEW[] = "${T_STO}/.nonew";
+    const char NEW_PL[] = "${T_STO}/.newpl";
+    const char CRONCOMMFILE[] = "${T_STO}/.tempcron";
+
+    char IPw0[] = $(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1);
+    char MACw0[] = $(ip link show wlan0 | awk '/ether/ {print $2}');
+    char IPe0[] = $(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f 1);
+    char MACe0[] = $(ip link show eth0 | awk '/ether/ {print $2}');
+
+    int retcode;
    printf("PATH : %s\n", getenv("PATH"));
    printf("HOME : %s\n", getenv("HOME"));
    printf("ROOT : %s\n", getenv("ROOT"));
