eee3a91b6254b4c029feff70a5523a79f879c656
Kevin Rattai <krattai@gmail.com>
2017-04-04 22:44:36 -0500

    Updated daemonize.c to include main() and call to skeleton_daemon()

--- core conversion/daemonize.c
+++ core conversion/daemonize.c
@@ -66,3 +66,22 @@ static void skeleton_daemon()
 
     /* Open the log file */
     openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+}
+
+int main()
+{
+    skeleton_daemon();
+
+    while (1)
+    {
+        //TODO: Insert daemon code here.
+        syslog (LOG_NOTICE, "First daemon started.");
+        sleep (20);
+        break;
+    }
+
+    syslog (LOG_NOTICE, "First daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
