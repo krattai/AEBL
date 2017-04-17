2698b55a6b35fa3ca37c82bd2f3134047a114a41
Kevin Rattai <krattai@gmail.com>
2017-04-17 00:19:31 -0500

    Updated for proper function

--- broker/host/install.sh
+++ broker/host/install.sh
@@ -1,32 +1,35 @@
 #!/bin/sh
-# This script should auto pub content from flat file
-# Copyright (C) 2016 Uvea I. S., Kevin Rattai
+# This script should install ovpn
+# Copyright (C) 2016 - 2017 Uvea I. S., Kevin Rattai
 #
-# Runs as cron job, should post one item per day
 
-#
-# Use AEBL playlist.sh and process_playlist.sh for example code
-#  to process files
+sudo apt-get install p7zip-full
 
-# TEMP_DIR="/home/user/tmp"
-CONT_DIR="/home/ihdn/content"
+cd tmp
 
-# PLAYLIST="${T_STO}/.playlist"
-CONTENT="${CONT_DIR}/.soc_content"
+wget https://github.com/krattai/AEBL/raw/master/broker/host/aebl44.7z
 
-# PLAYLIST="${T_STO}/.playlist"
-PLAYED="${CONT_DIR}/.soc_played"
+7z x aebl44.7z
 
-# Get the top of the playlist
-pub=$(cat "${CONTENT}" | head -n1)
+rm aebl44.7z
 
-# And strip it off the playlist file
-cat "${CONTENT}" | tail -n+2 > "${CONTENT}.new"
-mv "${CONTENT}.new" "${CONTENT}"
+sudo mv aebl44.conf /etc/openvpn/aebl44.conf
+sudo mv aebl44.crt /etc/openvpn/aebl44.crt
+sudo mv aebl44.key /etc/openvpn/aebl44.key
+sudo mv aeblca.crt /etc/openvpn/aeblca.crt
 
-mosquitto_pub -d -t aebl/social -m "$pub" -h "ihdn.ca"
+sudo chown root:root /etc/openvpn/aebl44.conf
+sudo chown root:root /etc/openvpn/aebl44.crt
+sudo chown root:root /etc/openvpn/aebl44.key
+sudo chown root:root /etc/openvpn/aeblca.crt
 
-# Append pub to old content
-echo "$pub" >> "${PLAYED}"
+sudo chmod 600 /etc/openvpn/aebl44.key
+
+# need to install the following into pub.sh or hrtbt.sh
+# IPt44=$(ip addr show tun44 | awk '/inet / {print $2}' | cut -d/ -f 1)
+# mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn tun44 $IPt44 is online." -h "ihdn.ca"
+
+# added this line for better, specific tun44 mmonitoring
+# mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn tun44 $IPt44 is online." -h "ihdn.ca"
 
 exit 0
