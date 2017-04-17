8261312df3531a33960805b6467b8ed3f9bfcd93
Kevin Rattai <krattai@gmail.com>
2017-04-17 00:17:33 -0500

    Added an installer for vpn

---
+++ broker/host/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# This script should auto pub content from flat file
+# Copyright (C) 2016 Uvea I. S., Kevin Rattai
+#
+# Runs as cron job, should post one item per day
+
+#
+# Use AEBL playlist.sh and process_playlist.sh for example code
+#  to process files
+
+# TEMP_DIR="/home/user/tmp"
+CONT_DIR="/home/ihdn/content"
+
+# PLAYLIST="${T_STO}/.playlist"
+CONTENT="${CONT_DIR}/.soc_content"
+
+# PLAYLIST="${T_STO}/.playlist"
+PLAYED="${CONT_DIR}/.soc_played"
+
+# Get the top of the playlist
+pub=$(cat "${CONTENT}" | head -n1)
+
+# And strip it off the playlist file
+cat "${CONTENT}" | tail -n+2 > "${CONTENT}.new"
+mv "${CONTENT}.new" "${CONTENT}"
+
+mosquitto_pub -d -t aebl/social -m "$pub" -h "ihdn.ca"
+
+# Append pub to old content
+echo "$pub" >> "${PLAYED}"
+
+exit 0
