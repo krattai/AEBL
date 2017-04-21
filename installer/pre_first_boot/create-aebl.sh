72ffe5bfdec3d2255efd502afa423f86e0042de4
Kevin Rattai <krattai@gmail.com>
2017-04-20 22:24:50 -0500

    Added remove of existing file to ensure fresh file

--- installer/pre_first_boot/create-aebl.sh
+++ installer/pre_first_boot/create-aebl.sh
@@ -71,6 +71,7 @@ while [ ! -f "${NETWORK_SYS}" ] && [ $net_wait -lt 10 ]; do
         echo "Internet available."
 
         # get, install, and run entertainment video script
+        rm instvident.sh
         wget -N -nd -w 3 --limit-rate=50k https://raw.githubusercontent.com/krattai/AEBL/master/installer/pre_first_boot/instvident.sh
         chmod 755 instvident.sh
         ./instvident.sh &
