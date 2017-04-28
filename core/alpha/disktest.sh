f333bae5a635901337c9de4f3d3c5118babd2ca1
Kevin Rattai <krattai@gmail.com>
2017-04-28 00:00:04 -0500

    Set check to be used on pi

--- core/alpha/disktest.sh
+++ core/alpha/disktest.sh
@@ -6,10 +6,13 @@
 #   7.2.2.2
 # This script does a very simple test for checking disk space.
 
-# key check is:
-#   df -h | awk '{print $5}' | grep % | grep -v Use | sort -n | tail -1 | cut -d "%" -f1 -
+# key checks:
+#   assuming interest in partition with most space used (not necessarily relevant; sorted to grab tail):
+#     df -h | awk '{print $5}' | grep % | grep -v Use | sort -n | tail -1 | cut -d "%" -f1 -
+#   pi / should be first drive, so no sorting as want first:
+#     df -h | awk '{print $5}' | grep % | grep -v Use | head -1 | cut -d "%" -f1 -
 
-space=`df -h | awk '{print $5}' | grep % | grep -v Use | sort -n | tail -1 | cut -d "%" -f1 -`
+space=`df -h | awk '{print $5}' | grep % | grep -v Use | head -1 | cut -d "%" -f1 -`
 alertvalue="80"
 
 if [ "$space" -ge "$alertvalue" ]; then
