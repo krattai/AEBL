aaf00e07d651fa7c18297e477cc0fd0a5863a57c
Kevin Rattai <krattai@gmail.com>
2017-05-14 00:07:50 -0500

    archived xtreemfs blade to current

--- archive/blades/xtreemfs/cpp/test/ld_preload/cp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-BUILD_DIR="../../build"
-#LD_DEBUG=all 
-XTREEMFS_PRELOAD_OPTIONS="--log-level DEBUG demo.xtreemfs.org/demo /xtreemfs" LD_PRELOAD=$BUILD_DIR"/libxtreemfs_preload.so" cp $1 $2
