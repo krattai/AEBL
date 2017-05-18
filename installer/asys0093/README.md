159d660056e065527930358e1539e32a17c777ba
Kevin Rattai <krattai@gmail.com>
2017-05-17 23:13:28 -0500

    Updated for new v0093

--- installer/asys0093/README.md
+++ installer/asys0093/README.md
@@ -1,5 +1,3 @@
-The AEBL core is the functional code that is required by an AEBL appliance to perform, for all intents and purposes, only the bare minimum activities, such as loading content onto the AEBL device and playing back that content.
+This directory contains the AEBL core scripts and code.
 
-The core also does contain code that allows a user to add and remove blades and interfaces, useful tools that enhance the functionality and ease of use of an AEBL appliance, if these tools had not been installed by way of a custom install.
-
-!* THIS IS THE WORKING DIRECTORY FOR CORE CODE ADDS AND UPDATES *!
+This is the final code for the basic, core AEBL device version, in this specific version release.  This code should NOT change, only the code in the core directory should be modified for the next patch or release.
--- installer/asys0093/inetup.sh
+++ installer/asys0093/inetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2015 - 2016 Uvea I. S., Kevin Rattai
+# Copyright (C) 2015 Uvea I. S., Kevin Rattai
 #
 # This script is a cron job script
 # It checks for network and internet connectivity
@@ -14,9 +14,6 @@
 # --tries=10
 # --timeout=20
 
-# Speaking of network, here is the proper way to set up raspbian static:
-# http://askubuntu.com/a/157192
-
 AEBL_TEST="/home/pi/.aebltest"
 AEBL_SYS="/home/pi/.aeblsys"
 IHDN_TEST="/home/pi/.ihdntest"
@@ -29,7 +26,6 @@ T_SCR="/run/shm/scripts"
 LOCAL_SYS="${T_STO}/.local"
 NETWORK_SYS="${T_STO}/.network"
 OFFLINE_SYS="${T_STO}/.offline"
-VPN_SYS="${T_STO}/.vpn_on"
 
 
 cd $HOME
@@ -103,45 +99,6 @@ fi
 #     sudo /etc/init.d/gogoc start
 # fi
 
-# Check VPN tun for master control IP
-# some possible examples of how to handle VPN down
-# if [ -f "${OFFLINE_SYS}" ]; then
-#     sudo ifdown eth0
-#     sudo ifdown wlan0
-#     sleep 5
-#     sudo ifup eth0
-#     sudo ifup wlan0
-#     sleep 10
-# fi
-# 
-# ping -c 1 10.8.0.1
-# 
-# if [ $? -eq 0 ]; then
-#     touch $NETWORK_SYS
-#     echo "Internet available."
-# else
-#     rm $NETWORK_SYS
-# fi
-
-# if script to be run on non-raspbian system, will likely need to set permissions:
-#     http://askubuntu.com/questions/167847/how-to-run-bash-script-as-root-with-no-password
-# specifically:
-#   + Make the file owned by root and group root:
-#   + sudo chown root.root <my script>
-#   + Now set the sticky but, make it executable for all and writable only by root:
-#   + sudo chmod 4755 <my script>
-#   + Keep in mind if this script will allow any input or editing of files, this will also be done as root.
-#
-# this works
-ping -c 1 10.8.0.1
-
-if [ $? -eq 0 ]; then
-    touch $VPN_SYS
-    echo "VPN available."
-else
-    rm $VPN_SYS
-    sudo service openvpn restart
-fi
 
 if [ -f "${NETWORK_SYS}" ]; then
     if [ ! -L /sys/class/net/tun ]; then
