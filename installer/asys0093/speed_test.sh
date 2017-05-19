079c616d4c054b209180b6b42f0aa23d083d88f7
Kevin Rattai <krattai@gmail.com>
2017-05-19 00:26:04 -0500

    updated for testing in new version 0093

--- installer/asys0093/speed_test.sh
+++ installer/asys0093/speed_test.sh
@@ -5,6 +5,6 @@ rm /home/pi/ctrl/speed.txt
 
 echo "internet speed:" > /home/pi/ctrl/speed.txt
  
-wget -O /dev/null https://github.com/krattai/AEBL/blob/master/core/stpkg.zip 2>&1 | grep '\([0-9.]\+ [KM]B/s\)' | sed -e 's|^.*(\([0-9.]\+ [KM]B/s\)).*$|\1|' >> /home/pi/ctrl/speed.txt
+wget -O /dev/null https://github.com/krattai/AEBL/raw/master/core/stpkg.zip 2>&1 | grep '\([0-9.]\+ [KM]B/s\)' | sed -e 's|^.*(\([0-9.]\+ [KM]B/s\)).*$|\1|' >> /home/pi/ctrl/speed.txt
 
 exit
