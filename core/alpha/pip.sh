568b836aa684d6c8a12f083ecfe6c684ed457765
Kevin Rattai <krattai@gmail.com>
2017-06-18 20:52:03 -0500

    Added info to note possible need to zero audio of pip

--- core/alpha/pip.sh
+++ core/alpha/pip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script should auto pub content from flat file
-# Copyright (C) 2016 Uvea I. S., Kevin Rattai
+# Copyright (C) 2016 - 2017 Uvea I. S., Kevin Rattai
 #
 # Plays pip (Picture In Picture)
 #
@@ -9,6 +9,7 @@
 
 mkfifo fifo
 chmod 777 fifo
+# not sure if important, but should probably set audio volume for pip to some form of near, if not, zero
 omxplayer --layer 2 --win "475 300 650 400" "mp4/Revolution OS.mp4" <fifo&
 echo -n "." > fifo
 
